040e414de08f9c59188ae23d1bdf40790e0c772c
Juan Carlos Ruiz <juancarlosruiz14@gmail.com>
2014-12-16 19:15:50 -0600

    first commit

---
+++ client/templates/calendar/calendar.js
@@ -0,0 +1,41 @@
+Session.setDefault('editing_calevent', null);
+Session.setDefault('showEditEvent', false);
+Session.setDefault('lastMod', null);
+
+Template.calendar.rendered = function () 
+{
+	$('#calendar').fullCalendar({
+		dayClick: function(date, jsEvent, view) 
+		{						
+			CalEvents.insert({title:'New Event',start:date.format(),end:date.format()});
+			Session.set('lastMod', new Date());        
+		},
+		eventClick: function(calEvent, jsEvent, view) 
+		{
+		},
+		events: function(start, end, timezone, callback) 
+		{			
+			var events =[];
+			calEvents =CalEvents.find();
+			calEvents.forEach(function (evt) {
+				events.push({
+					id:evt._id,
+					title:evt.title,
+					start:evt.start,
+					end:evt.end
+				});
+			});
+
+			callback(events);    	
+		}
+
+	});
+}
+
+Template.calendar.helpers({
+	lastMod: function () {
+		console.log(Session.get('lastMod'));
+		return Session.get('lastMod');
+	}
+});
+
