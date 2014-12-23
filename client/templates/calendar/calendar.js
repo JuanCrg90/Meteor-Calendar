a27a8fc9472b4a95823932d9d3f3d3f099fcab2c
Juan Carlos Ruiz <juancarlosruiz14@gmail.com>
2014-12-23 13:48:56 -0600

    added update event and drag&drop event

--- client/templates/calendar/calendar.js
+++ client/templates/calendar/calendar.js
@@ -5,18 +5,19 @@ Session.setDefault('lastMod', null);
 Template.calendar.rendered = function () 
 {
 	$('#calendar').fullCalendar({
-		dayClick: function(date, jsEvent, view) 
-		{						
+		dayClick: function(date, jsEvent, view){						
 			CalEvents.insert({title:'New Event',start:date.format(),end:date.format()});
 			Session.set('lastMod', new Date());        
 		},
-		eventClick: function(calEvent, jsEvent, view) 
-		{
-			Session.set('editing_calevent',calEvent.id)
-			Session.set('showEditEvent',true)
+		eventClick: function(calEvent, jsEvent, view){
+			Session.set('editing_calevent',calEvent.id);
+			Session.set('showEditEvent',true);
 		},
-		events: function(start, end, timezone, callback) 
-		{			
+		eventDrop: function(calEvent){
+			//console.log(calEvent.id,calEvent.start.format(),calEvent.end.format());
+			CalEvents.update(calEvent.id,{$set: {start:calEvent.start.format(), end:calEvent.end.format()}})
+		},
+		events: function(start, end, timezone, callback){			
 			var events =[];
 			calEvents =CalEvents.find();
 			calEvents.forEach(function (evt) {
@@ -29,7 +30,9 @@ Template.calendar.rendered = function ()
 			});
 
 			callback(events);    	
-		}
+		},
+		editable: true
+
 
 	});
 }
@@ -51,9 +54,23 @@ Template.calendar.events({
 });
 
 Template.editEvent.helpers({
-	title: function () {
-		var calEvent =CalEvents.findOne({id:Session.get('editing_calevent')});
-		console.log(calEvent)
-		return calEvent
+	evt: function () {
+		var calEvent =CalEvents.findOne({_id:Session.get('editing_calevent')});		
+		return calEvent;
 	}
 });
+
+Template.editEvent.events({
+	'click #save': function (evt,tmpl) {
+		updateCalEvent(Session.get('editing_calevent'),tmpl.find('#title').value);
+		Session.set('editing_calevent',null);
+			Session.set('showEditEvent',false);
+		
+		
+	}
+});
+
+var updateCalEvent=function(id,title){
+	CalEvents.update(id, {$set: {title:title}});
+	return true;
+}
